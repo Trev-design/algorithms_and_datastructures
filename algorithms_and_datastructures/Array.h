e957a01576265c596094b262e2c4b71217ab992e
Tev-design <gerrit.flick@gmail.com>
2023-12-27 10:04:40 +0100

    basic functionalities of an dynamic array

--- algorithms_and_datastructures/Array.h
+++ algorithms_and_datastructures/Array.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#include<stdexcept>
+#include<iostream>
+
 template<typename Type>
 class Array
 {
@@ -10,7 +14,67 @@ private:
 public:
 	Array() noexcept : mCapacity(1), mSize(0), moptrData(new Type[mCapacity]) {}
 	Array(const size_t capacity) noexcept : mCapacity(capacity), mSize(0), moptrData(new Type[capacity]) {}
+	~Array() noexcept { delete[] moptrData; }
 
+	inline size_t Size() const { return mSize; }
 
+	Type& operator [] (size_t index);
+	const Type operator [] (size_t index) const;
+
+	void Push(const Type& item);
+	Type Pop();
+private:
+	Type GetItem(size_t index) const;
+	void Resize(size_t newCapacity);
 };
 
+template<typename Type>
+inline Type& Array<Type>::operator[](size_t index)
+{
+	return GetItem(index);
+}
+
+template<typename Type>
+inline const Type Array<Type>::operator[](size_t index) const
+{
+	return GetItem(index);
+}
+
+template<typename Type>
+inline void Array<Type>::Push(const Type& item)
+{
+	moptrData[mSize] = item;
+	++mSize;
+}
+
+template<typename Type>
+inline Type Array<Type>::Pop()
+{
+	if (mSize <= 0) { throw std::underflow_error("No items in this array"); }
+	if (mSize < (mCapacity / 4)) { Resize(mCapacity / 2); }
+
+	Type item = moptrData[mSize];
+	--mSize;
+	return item;
+}
+
+template<typename Type>
+inline Type Array<Type>::GetItem(size_t index) const
+{
+	if (index > mSize) { throw std::overflow_error("index to high"); }
+	return moptrData[index];
+}
+
+template<typename Type>
+inline void Array<Type>::Resize(size_t newCapacity)
+{
+	Type* newArray = new Type[new newCapacity];
+	for (size_t index = 0; index < mSize, ++index)
+	{
+		newArray[index] = moptrData[index];
+	}
+
+	delete[] moptrData;
+	mCapacity = newCapacity;
+	moptrData = newArray;
+}
